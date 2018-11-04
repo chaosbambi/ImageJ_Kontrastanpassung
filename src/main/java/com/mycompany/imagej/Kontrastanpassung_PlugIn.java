c7886113e376b9b22a99b2ccde07ab2dff1f358f
Janfiderheld <Janfitoe@web.de>
2018-11-04 16:16:19 +0100

    Dialog zur Werteeingabe
    - Dialog zur Abfrage des min- & max-Wertes eingefügt (akt. 2 Dialoge)
    - Kontrolle ob min <= max-Wert ist sonst Meldung an Nutzer

--- src/main/java/com/mycompany/imagej/Kontrastanpassung_PlugIn.java
+++ src/main/java/com/mycompany/imagej/Kontrastanpassung_PlugIn.java
@@ -1,47 +1,64 @@
 package com.mycompany.imagej;
 
+import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
 public class Kontrastanpassung_PlugIn implements PlugInFilter {
-
-	int color_min=0;
-	int color_max=255;
+    
+    final int COLOR_MIN_DEFAULT = 0;
+    final int COLOR_MAX_DEFAULT = 255;
+    int color_min, color_max;
 	
-public int setup(String args, ImagePlus im) {
-	return DOES_8G; // this plugin accepts 8-bit grayscale images
-}
+    @Override
+    public int setup(String args, ImagePlus im) {   
+        return DOES_8G; // this plugin accepts 8-bit grayscale images
+    }
 
-public void run(ImageProcessor ip) {
-	int M = ip.getWidth();
-	int N = ip.getHeight();
+    @Override
+    public void run(ImageProcessor ip) {
+        int M = ip.getWidth();
+        int N = ip.getHeight();
 
-	int color_low=255;
-	int color_high=0;
-	
-	// iterate over all image coordinates (u,v)
-	for (int u = 0; u < M; u++) {
-		for (int v = 0; v < N; v++) {
-			int p = ip.getPixel(u, v);
-			//get maximum and minimum
-			if ( p < color_low) {
-				color_low = p;
-			}
-			if(p>color_high) {
-				color_high = p;
-			}
-		}
-	}
-	int new_p;
-	// iterate over all image coordinates (u,v)
-	for (int u = 0; u < M; u++) {
-		for (int v = 0; v < N; v++) {
-			int p = ip.getPixel(u, v);
-			//use linear contrastmodifyer
-			new_p = color_min + (p-color_low)*(color_max-color_min)/(color_high-color_low);
-			ip.putPixel(u, v, new_p);
-		}
-	}
-}
+        int color_low = 255;
+        int color_high = 0;
+
+        // let the user input the new min and max value for the image
+        color_min = (int) IJ.getNumber("Please enter the minimal value (at least 0): ", COLOR_MIN_DEFAULT);
+        color_max = (int) IJ.getNumber("Please enter the maximal value (less than 255): ", COLOR_MAX_DEFAULT);
+               
+        // check if the minimal value is less than the maximal value
+        // TODO: Check if really necessary
+        if (color_min >= color_max) {
+            IJ.showMessage("Error", "The minimal value is higher than the maximal value!\nThe Default Values (0 & 255) will be used");
+            color_min = COLOR_MIN_DEFAULT;
+            color_max = COLOR_MAX_DEFAULT;                    
+        }
+
+        // iterate over all image coordinates (u,v)
+        for (int u = 0; u < M; u++) {
+            for (int v = 0; v < N; v++) {
+                int p = ip.getPixel(u, v);
+                //get maximum and minimum
+                if (p < color_low) {
+                        color_low = p;
+                }
+                if (p > color_high) {
+                        color_high = p;
+                }
+            }
+        }
+
+        int new_p;
+        // iterate over all image coordinates (u,v)
+        for (int u = 0; u < M; u++) {
+            for (int v = 0; v < N; v++) {
+                int p = ip.getPixel(u, v);
+                //use linear contrastmodifyer
+                new_p = color_min + (p-color_low)*(color_max-color_min)/(color_high-color_low);
+                ip.putPixel(u, v, new_p);
+            }
+        }
+    }
 }
